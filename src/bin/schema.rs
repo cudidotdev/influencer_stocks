b82df628ae56a7a75f0d714582eb6a18c69f8d8a
Cudilala <augustinemadu9@gmail.com>
2025-03-13 21:06:43 +0100

    project init from template

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use influencer_stocks::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
