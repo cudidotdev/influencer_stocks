a46e9218ff3af595fd709c5451f4f66e36783fc5
Cudilala <augustinemadu9@gmail.com>
2025-03-19 17:20:24 +0100

    fix errors

--- contract/src/contract.rs
+++ contract/src/contract.rs
@@ -11,7 +11,7 @@ pub mod execute;
 pub mod query;
 
 // Denomination of the token we're using
-pub const DENOM: &str = "uosmo";
+pub const DENOM: &str = "uhuahua";
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:influencer-stocks";
@@ -243,41 +243,5 @@ pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
         QueryMsg::GetSalesById { sale_id } => {
             to_json_binary(&query::sales::get_sale_by_id(deps, env, sale_id)?)
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::utils;
-    use cosmwasm_std::coins;
-    use cosmwasm_std::testing::{message_info, mock_dependencies, mock_env};
-
-    #[test]
-    fn proper_initialization() {
-        let mut deps = mock_dependencies();
-
-        let creator = deps.api.addr_make("creator");
-
-        let msg = InstantiateMsg {};
-
-        let info = message_info(&creator, &coins(1000, "token"));
-
-        let sender = info.sender.clone();
-
-        // we can just call .unwrap() to assert this was a success
-        let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        assert!(utils::response::contains_attribute(
-            &res,
-            "method",
-            "instantiate"
-        ));
-
-        assert!(utils::response::contains_attribute(
-            &res,
-            "owner",
-            sender.as_str()
-        ));
     }
 }
